1563617fe3a5858a1ee4fd5d543aa7020c216f50
Jacob Barrieault <jacob@quikly.com>
2016-05-02 07:09:17 -0400

    port ruby script to burndown.js

--- javascript/burndown.js
+++ javascript/burndown.js
@@ -21,64 +21,11 @@ $(document).ready(function() {
 
   $('#debt-form').on('submit', function(e){
     e.preventDefault();
-    data  = {}
-    debts = getDebts(this)
-
-    datasets      = buildDebtDatasets(debts)
-    data.datasets = datasets
-    data.labels   = getRepaymentMonths(datasets)
-    // buildBurndownChart(data);
-
-    // var data = {
-    //   labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October"],
-    //   datasets: [
-    //     {
-    //       label: "Loan Name",
-    //       fill: true,
-    //       stacked: true,
-    //       lineTension: 0.1,
-    //       backgroundColor: "rgba(75,192,192,0.4)",
-    //       borderColor: "rgba(75,192,192,1)",
-    //       borderCapStyle: 'butt',
-    //       borderDash: [],
-    //       borderDashOffset: 0.0,
-    //       borderJoinStyle: 'miter',
-    //       pointBorderColor: "rgba(75,192,192,1)",
-    //       pointBackgroundColor: "#fff",
-    //       pointBorderWidth: 1,
-    //       pointHoverRadius: 5,
-    //       pointHoverBackgroundColor: "rgba(75,192,192,1)",
-    //       pointHoverBorderColor: "rgba(220,220,220,1)",
-    //       pointHoverBorderWidth: 2,
-    //       pointRadius: 1,
-    //       pointHitRadius: 10,
-    //       data: [13844, 11839, 9382, 7943, 6819, 4329, 2975, 758, 0],
-    //     },
-    //     {
-    //       label: "Other Loan Name",
-    //       fill: true,
-    //       stacked: true,
-    //       lineTension: 0.1,
-    //       backgroundColor: "rgba(255,99,132,0.2)",
-    //       borderColor: "rgba(255,99,132,1)",
-    //       borderCapStyle: 'butt',
-    //       borderDash: [],
-    //       borderDashOffset: 0.0,
-    //       borderJoinStyle: 'miter',
-    //       pointBorderColor: "rgba(255,99,132,0.4)",
-    //       pointBackgroundColor: "#fff",
-    //       pointHoverRadius: 5,
-    //       pointHoverBackgroundColor: "rgba(255,99,132,0.4)",
-    //       pointHoverBorderColor: "rgba(255,99,132,1)",
-    //       pointHoverBorderWidth: 2,
-    //       pointRadius: 1,
-    //       pointHitRadius: 10,
-    //       data: [13341, 11777, 9821, 7911, 6895, 4329, 2975, 758, 200, 0],
-    //     },
-    //   ],
-    // }
-
-    // buildBurndownChart(data);
+    debts         = getDebts(this)
+    if (debts != []) {
+      chartData     = buildChartData(debts)
+      buildBurndownChart(chartData);
+    }
   });
 
   function buildBurndownChart(data) {
@@ -108,24 +55,89 @@ $(document).ready(function() {
 
   function getDebts(form) {
     formData = $(form).serializeArray()
-    return _.object(formData.map(function(v) {return [v.name, v.value];} ))
+    // console.log(_.object(formData.map(function(v) {return [v.name, v.value];} )))
+    // return _.object(formData.map(function(v) {return [v.name, v.value];} ))
+
+    debts = []
+    _.each($('.debt-info'), function(el) {
+      name = $(el).find('[name="name"').val()
+      amount = $(el).find('[name="amount"').val()
+      monthlyPayment = $(el).find('[name="monthlyPayment"').val()
+      if (name != '' && amount != '' && monthlyPayment != '') {
+        debts.push({name: name, amount: amount, monthlyPayment: monthlyPayment})
+      }
+      return debts
+    });
+    return debts
   }
 
-  function getRepaymentMonths(debts) {
-    months = []
-    debts
+  function buildChartData(debts) {
+    monthlyPaymentTotal = _.inject(debts, function(sum, debt) { return sum + debt.monthlyPayment })
+    remainingDebts = _.reject(debts, function(debt) { return debt.amount == 0});
+    remainingDebts = _.sortBy(remainingDebts, function(debt){ return debt.amount })
+    totalDebt      = _.inject(debts, function(sum, debt) { return sum + debt.amount })
 
+    datasets = {}
+    _.each(debts, function(debt) {
+      datasets[debt.name] = { monthlyBalances: [] }
+    });
+
+    date     = moment()
+    labels   = []
+    leftover = 0
+
+    while (totalDebt != 0) {
+      remainingDebts = _.reject(remainingDebts, function(debt) { return debt.amount == 0});
+      remainingDebts = _.sortBy(remainingDebts, function(debt) { return debt.amount })
+
+      _.each(remainingDebts, function(debt){
+        // use leftover money
+        // TODO leftover doesn't seem to be being paid towards next-highest loan
+        leftoverPayment = Math.min(debt.amount, leftover)
+        debt.amount     = debt.amount - leftoverPayment
+        leftover        = leftover - leftoverPayment
+
+        // make monthly payment
+        payment = Math.min(debt.amount, debt.monthlyPayment)
+        debt.amount = debt.amount - payment
+        leftover = leftover + (debt.monthlyPayment - payment)
+        datasets[debt.name].monthlyBalances.push(debt.amount)
+      });
+
+      debtTotals = _.map(remainingDebts, function(debt) { return debt.amount})
+      totalDebt = _.inject(debtTotals, function(sum, debt) { return sum + debt.amount })
+      labels.push(date.format('MMMM / YYYY'))
+      date.add(1, 'months')
+    }
+
+    return {labels: labels, datasets: buildChartDatasets(datasets) }
   }
 
-  function buildDebtDatasets(debts) {
-    // datasets = []
-    // for debt in debts {
-    //   datasets << buildDebtDatasets(debts)
-    // }
-    // return datasets
-  }
+  function buildChartDatasets(debts) {
+    datasets = _.map(debts, function(data, name) {
+      return {
+        label: name,
+        fill: true,
+        stacked: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(255,99,132,0.4)",
+        pointBackgroundColor: "#fff",
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(255,99,132,0.4)",
+        pointHoverBorderColor: "rgba(255,99,132,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: data.monthlyBalances,
+      }
+    });
 
-  function buildDebtDataset(debt) {
     // need to generate colors too
     // {
     //   label: debt.name,
@@ -148,5 +160,6 @@ $(document).ready(function() {
     //   pointHitRadius: 10,
     //   data: debt.history,
     // }
+    return datasets
   }
 });
